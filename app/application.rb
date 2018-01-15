e7acb2315f5c3d4f771958ddc0b92c431de671f6
Meredith Reece <mardebugger@gmail.com>
2018-01-15 19:37:50 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -21,6 +21,13 @@ class Application
           resp.write "#{item}\n"
         end
       end
+    elsif req.path.match(/add/)
+      item_to_add = req.params["item"]
+      if @@items.include?(item_to_add)
+        @@cart << item_to_add
+      else
+        resp.write "We don't have that item"
+      end
     else
       resp.write "Path Not Found"
     end
